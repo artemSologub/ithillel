015cf35e40b4f840a6bd44f697621ea04010a394
Artem <artem.sologub92@gmail.com>
2024-05-02 16:45:10 +0300

    file_sync refactoring. File name in log. gitignore update.

--- file_sync.js
+++ file_sync.js
@@ -3,69 +3,72 @@ const path = require('path');
 
 const logger = require('./utils/logger')('file_sync');
 
-function fileSync() {
-  return {
-    start: () => {
-      const sourceFolder = path.join('.', 'source');
-      const targetFolder = path.join('.', 'target');
+function start() {
+  const sourceFolder = path.join('.', 'source');
+  const targetFolder = path.join('.', 'target');
 
-      fileSync().copyCatalog(sourceFolder, targetFolder);
-    },
-    copyCatalog: (source, target) => {
-      fs.readdir(source, (err, files) => {
-        if (err) {
-          logger.error('Error reading source directory:', err);
-          return;
-        }
+  copyCatalog(sourceFolder, targetFolder);
+}
 
-        // Check if the folder exists
-        fs.access(target, fs.constants.F_OK, (err) => {
+function copyCatalog(source, target) {
+  fs.readdir(source, (err, files) => {
+    if (err) {
+      logger.error('Error reading source directory:', err);
+      return;
+    }
+
+    // Check if the folder exists
+    fs.access(target, fs.constants.F_OK, (err) => {
+      if (err) {
+        // Create if not
+        fs.mkdir(target, { recursive: true }, (err) => {
           if (err) {
-            // Create if not
-            fs.mkdir(target, { recursive: true }, (err) => {
+            logger.error('Error creating target folder:', err);
+            return;
+          }
+          copyFiles(files, source, target);
+        });
+      } else {
+        copyFiles(files, source, target);
+      }
+    });
+  });
+}
+
+function copyFiles(files, source, target) {
+  files.forEach((file) => {
+    const sourcePath = path.join(source, file);
+    const targetPath = path.join(target, file);
+    fs.stat(sourcePath, (err, stats) => {
+      if (err) {
+        logger.error('Error stating file:', err);
+        return;
+      }
+
+      if (stats.isDirectory()) {
+        copyCatalog(sourcePath, targetPath);
+      } else {
+        fs.exists(targetPath, (exists) => {
+          if (exists) {
+            logger.warn(`File [${file}] exists!`);
+          } else {
+            fs.copyFile(sourcePath, targetPath, (err) => {
               if (err) {
-                logger.error('Error creating target folder:', err);
+                logger.error('Error copying file:', err);
                 return;
               }
-              copyFiles(files);
+              logger.info('File copied successfully:', targetPath);
             });
-          } else {
-            copyFiles(files);
           }
         });
+      }
+    });
+  });
+}
 
-        function copyFiles(files) {
-          files.forEach((file) => {
-            const sourcePath = path.join(source, file);
-            const targetPath = path.join(target, file);
-            fs.stat(sourcePath, (err, stats) => {
-              if (err) {
-                logger.error('Error stating file:', err);
-                return;
-              }
-
-              if (stats.isDirectory()) {
-                fileSync().copyCatalog(sourcePath, targetPath);
-              } else {
-                fs.exists(targetPath, (exists) => {
-                  if (exists) {
-                    logger.warn('File exists!');
-                  } else {
-                    fs.copyFile(sourcePath, targetPath, (err) => {
-                      if (err) {
-                        logger.error('Error copying file:', err);
-                        return;
-                      }
-                      logger.info('File copied successfully:', targetPath);
-                    });
-                  }
-                });
-              }
-            });
-          });
-        }
-      });
-    },
+function fileSync() {
+  return {
+    start: () => start(),
   };
 }
 
